5d5b495474b3b189492e041776d39bbb347e41e4
bianhao123 <2495067728@qq.com>
2024-03-26 20:05:11 +0800

    fix: ddp scripts

--- train_multigpu.sh
+++ train_multigpu.sh
@@ -11,7 +11,7 @@ gpu_count=$(echo $gpu_ids | tr -cd ',' | wc -c)
 gpu_count=$((gpu_count + 1))
 
 # pytorch1.x
-CUDA_VISIBLE_DEVICES=$gpu_ids python -m torch.distributed.launch --nproc_per_node=$gpu_count --master_port=4343 basicsr/train.py --opt $config --launcher pytorch
+CUDA_VISIBLE_DEVICES=$gpu_ids python -m torch.distributed.launch --nproc_per_node=$gpu_count --master_port=$master_port basicsr/train.py --opt $config --launcher pytorch
 
 # pytorch2.x
 # CUDA_VISIBLE_DEVICES=$gpu_ids torchrun --nproc_per_node=$gpu_count --master_port=$master_port basicsr/train_ddp.py --opt $config --launcher pytorch
